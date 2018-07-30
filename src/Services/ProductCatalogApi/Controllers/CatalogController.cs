ec1acf9daded844639f1f3841adf98fdc15fcda1
Nola Alzayat <nola@MacBook-Pro-7.local>
2018-07-30 10:25:50 -0400

    Completed 3.17

---
+++ src/Services/ProductCatalogApi/Controllers/CatalogController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ShoesOnContainers.Services.ProductCatalogApi;
+using ShoesOnContainers.Services.ProductCatalogApi.Data;
+
+namespace ProductCatalogApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Catalog")]
+    public class CatalogController : Controller
+    {
+        private readonly CatalogContext _catalogContext;
+        private readonly IOptionsSnapshot<CatalogSettings> _settings;
+
+        // Inject instance of catalog settings
+        public CatalogController(CatalogContext catalogContext, IOptionsSnapshot<CatalogSettings> settings)
+        {
+            _catalogContext = catalogContext;
+            _settings = settings;
+            ((DbContext)catalogContext).ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+
+        }
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CatalogTypes()
+        {
+            var items = await _catalogContext.CatalogTypes.ToListAsync();
+            return Ok(items);
+
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CatalogBrands()
+        {
+            var items = await _catalogContext.CatalogBrands.ToListAsync();
+            return Ok(items);
+
+        }
+
+    }
+}
