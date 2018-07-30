7f2348a4882382a2f3bb6394f6138671f34baa40
Nola Alzayat <nola@MacBook-Pro-7.local>
2018-07-30 14:52:56 -0400

    Fixed build

--- src/Services/ProductCatalogApi/Controllers/CatalogController.cs
+++ src/Services/ProductCatalogApi/Controllers/CatalogController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using ProductCatalogApi.Data;
 using ShoesOnContainers.Services.ProductCatalogApi;
 using ShoesOnContainers.Services.ProductCatalogApi.Data;
 
